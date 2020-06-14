01e424e360d7b3233b1dd6256e0b604871c0b78c
generyk <erykrachwal@yahoo.com>
2020-06-14 09:22:56 -0400

    user create method update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ def create
   @user = User.new(user_params)
   if @user.save
     session[:user_id] = @user.id
-    redirect_to @user
+    redirect_to user_path(@user)
   else
     render :new
   end
@@ -17,7 +17,7 @@ end
 def show
 end
 
-private 
+private
 
 def user_params
   params.require(:user).permit(:username, :email, :password)
